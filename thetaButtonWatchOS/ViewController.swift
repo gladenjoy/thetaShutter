ecc76f13328036736b0ef5281ba6d3c2cd87482b
gladenjoy <gladenjoy.is01@gmail.com>
2016-01-02 16:27:07 +0900

    Initial Commit

---
+++ thetaButtonWatchOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  thetaButtonWatchOS
+//
+//  Created by 喜楽智規 on 2016/01/02.
+//  Copyright © 2016年 喜楽智規. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
